dc59e647c0a5a8c8146c04182cea72a4902b11e9
Angel Black <53869613+angelBlackRe@users.noreply.github.com>
2020-10-11 17:11:37 -0500

    Update principalLibrero.py

--- 09102020/principalLibrero.py
+++ 09102020/principalLibrero.py
@@ -3,6 +3,8 @@ from disco import Disco
 from revista import Revista
 from librero import Librero
 from comic import Comic
+from manga import Manga
+
 miLibrero = Librero()
 
 miLibrero.agregaLibro(
@@ -22,18 +24,16 @@ miLibrero.agregaRevista(Revista(nombreR="Muy interesante", articulosR="Ciencia, 
                                 editorialR="Zinet Media Global", fechaEmision="enero 2020"))
 miLibrero.agregarComic(Comic(autor="Gerard Way", titulo="The umbrella academy",
                              anio="2018", editorial="Dark Horse Comics"))
-#miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
+# Manga
+miLibrero.agregarManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="Panni"))
+
+miLibrero.agregarManga(Manga(titulo="Suicide boy",autor="ParkGee",numero="57",tomo="2",editorial="Lezhin"))
+
+miLibrero.agregarManga(Manga(titulo="Platinum end",autor="anonimo",numero="35",tomo="1",editorial="Panni"))
+
+miLibrero.agregarManga(Manga(titulo="Attack on titan",autor="Hajime Isayama",numero="107",tomo="8",editorial="Panni"))
+
+miLibrero.agregarManga(Manga(titulo="All you need is kill",autor="anonimo",numero="3",tomo="1",editorial="Panni"))
+
 miLibrero.muestra()
 
-# manga
-##from manga import Manga
-#sb = Manga("Platinum end", "Panni")
-# sb.manwha()
-#tg = Manga(titulo="swzSuicide boy",autor="ParkGee",numero="57",tomo="2",editorial="Lezhin")
-# tg.manwha()
-#miLibrero = Librero()
-# miLibrero.agregaManga(sb)
-# miLibrero.agregaManga(tg)
-#miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
-# Debes crear tu clase MANGA
-# miLibrero.manwha()
