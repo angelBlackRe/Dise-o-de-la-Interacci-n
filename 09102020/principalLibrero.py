d5708ad2b8b513d4fad7b7df38e591e748918437
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2020-10-11 16:22:14 -0500

    add

--- 09102020/principalLibrero.py
+++ 09102020/principalLibrero.py
@@ -9,6 +9,7 @@ miLibrero.agregaLibro(Libro(autor="korth", titulo="bases", editorial="Pearson"))
 miLibrero.agregaDisco(Disco(interprete="Ricardo Montaner", titulo="Vuelve Conmigo", album="Ida y Vuelta Edición Especial", anio=2017))
 miLibrero.agregaRevista(Revista(nombreR="Muy interesante", articulosR="Ciencia, Salud, Tecnologia", editorialR="Zinet Media Global",fechaEmision="enero 2020"))
 miLibrero.agregarComic(Comic(autor="Gerard Way",titulo="The umbrella academy", anio="2018",editorial="Dark Horse Comics"))
+#miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
 miLibrero.muestra()
 
 #manga
@@ -21,5 +22,5 @@ miLibrero.muestra()
 #miLibrero.agregaManga(sb)
 #miLibrero.agregaManga(tg)
 #miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
-#
+# Debes crear tu clase MANGA
 #miLibrero.manwha()
