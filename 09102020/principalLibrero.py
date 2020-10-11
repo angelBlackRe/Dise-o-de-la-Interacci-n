01d83611d600c1874f887ce6ee4a9e4f91fa7f82
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2020-10-11 16:37:53 -0500

    Update principalLibrero.py

--- 09102020/principalLibrero.py
+++ 09102020/principalLibrero.py
@@ -5,22 +5,35 @@ from librero import Librero
 from comic import Comic
 miLibrero = Librero()
 
-miLibrero.agregaLibro(Libro(autor="korth", titulo="bases", editorial="Pearson"))
-miLibrero.agregaDisco(Disco(interprete="Ricardo Montaner", titulo="Vuelve Conmigo", album="Ida y Vuelta Edición Especial", anio=2017))
-miLibrero.agregaRevista(Revista(nombreR="Muy interesante", articulosR="Ciencia, Salud, Tecnologia", editorialR="Zinet Media Global",fechaEmision="enero 2020"))
-miLibrero.agregarComic(Comic(autor="Gerard Way",titulo="The umbrella academy", anio="2018",editorial="Dark Horse Comics"))
+miLibrero.agregaLibro(
+    Libro(autor="korth", titulo="bases", editorial="Pearson"))
+miLibrero.agregaDisco(Disco(interprete="Ricardo Montaner",
+                            titulo="Vuelve Conmigo", album="Ida y Vuelta Edición Especial", anio=2017))
+miLibrero.agregaDisco(Disco(interprete="Michael Jackson",
+                            titulo="Bllie Jean", album="Thriller", anio=1983))
+miLibrero.agregaDisco(Disco(interprete="Zara Larsson",
+                            titulo="Lush Life", album="So Good", anio=2017))
+miLibrero.agregaDisco(Disco(interprete="Hailee Steinfeld",
+                            titulo="Love Myself", album="Haiz", anio=2015))
+miLibrero.agregaDisco(Disco(interprete="Carly Rae Jepsen",
+                            titulo="I Really Like You", album="Emotion", anio=2015))
+
+miLibrero.agregaRevista(Revista(nombreR="Muy interesante", articulosR="Ciencia, Salud, Tecnologia",
+                                editorialR="Zinet Media Global", fechaEmision="enero 2020"))
+miLibrero.agregarComic(Comic(autor="Gerard Way", titulo="The umbrella academy",
+                             anio="2018", editorial="Dark Horse Comics"))
 #miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
 miLibrero.muestra()
 
-#manga
+# manga
 ##from manga import Manga
 #sb = Manga("Platinum end", "Panni")
-#sb.manwha()
+# sb.manwha()
 #tg = Manga(titulo="swzSuicide boy",autor="ParkGee",numero="57",tomo="2",editorial="Lezhin")
-#tg.manwha()
+# tg.manwha()
 #miLibrero = Librero()
-#miLibrero.agregaManga(sb)
-#miLibrero.agregaManga(tg)
+# miLibrero.agregaManga(sb)
+# miLibrero.agregaManga(tg)
 #miLibrero.agregaManga(Manga(autor="Sui ishida", titulo="Tokyo Ghoul: re", editorial="panni"))
 # Debes crear tu clase MANGA
-#miLibrero.manwha()
+# miLibrero.manwha()
