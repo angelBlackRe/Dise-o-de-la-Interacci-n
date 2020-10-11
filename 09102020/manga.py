2921f2110c3c493c540ccf37aceec89166b74751
Angel Black <53869613+angelBlackRe@users.noreply.github.com>
2020-10-11 17:17:20 -0500

    Create manga.py

---
+++ 09102020/manga.py
@@ -0,0 +1,17 @@
+# Clase Manga
+class Manga:
+    def __init__(self, titulo="", autor="", numero="", tomo="", editorial=""):
+
+        self.tituloM = tituloM
+        self.autorM = autorM
+        self.numeroM = numeroM
+        self.tomoM = tomoM
+        self.editorialM = editorialM
+
+    def manwha(self):
+        print(80*"-")
+        print("El manga: " + self.tituloM)
+        print("del autor: " + self.autorM)
+        print("número: " + self.numeroM)
+        print("tomo: " + self.tomoM)
+        print("editorial: " + self.editorialM)
